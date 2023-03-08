f3d0696ac8a4ccf5950c8be611caab416615356c
taiddpd08204 <taiddpd08204@fpt.edu.vn>
2023-03-08 17:53:14 +0700

    lab

---
+++ lab1/app.js
@@ -0,0 +1,12 @@
+// Nhập vào 2 số nguyên a, b từ bàn phím
+let a = parseInt(prompt("Nhập số nguyên a:"));
+let b = parseInt(prompt("Nhập số nguyên b:"));
+
+// Tính tổng c của a và b
+let c = a + b;
+
+// In ra kết quả đồng thời bằng 2 cách
+document.write("Kết quả tính tổng của " + a + " và " + b + " là: " + c + "<br>");
+console.log("Kết quả tính tổng của " + a + " và " + b + " là: " + c);
+
+// Liên kết vào file lab1.html
