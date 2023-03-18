2bba252463e635170c32009d9ef0df9f14e15940
taiddpd08204 <taiddpd08204@fpt.edu.vn>
2023-03-18 20:17:13 +0700

    hehe

--- lab3/app2.js
+++ lab3/app2.js
@@ -1,66 +1,68 @@
 // Hàm tính điểm trung bình
 function tinhDiemTrungBinh() {
-    // Nhập điểm Toán, Lý, Hóa, Sinh
-    let diemToan =prompt("Nhập điểm Toán:");
-    let diemLy =prompt("Nhập điểm Lý:");
-    let diemHoa =prompt("Nhập điểm Hóa:");
-    let diemSinh =prompt("Nhập điểm Sinh:");
-  
-    // Kiểm tra điểm đầu vào
-    if (diemToan >= 0 && diemToan <= 10 &&
-        diemLy >= 0 && diemLy <= 10 &&
-        diemHoa >= 0 && diemHoa <= 10 &&
-        diemSinh >= 0 && diemSinh <= 10) {
-  
-      // Tính điểm trung bình
-      let diemTrungBinh = (diemToan + diemLy + diemHoa + diemSinh) / 4;
-  
-      // Xuất kết quả xếp loại
-      if (diemTrungBinh >= 9 && diemTrungBinh <= 10) {
-        console.log("Xếp loại: Giỏi");
-      } else if (diemTrungBinh >= 7 && diemTrungBinh < 9) {
-        console.log("Xếp loại: Khá");
-      } else if (diemTrungBinh >= 5 && diemTrungBinh < 7) {
-        console.log("Xếp loại: Trung Bình");
-      } else {
-        console.log("Xếp loại: Yếu");
-      }
-  
+  // Nhập điểm Toán, Lý, Hóa, Sinh
+  let diemToan = parseFloat(prompt("Nhập điểm Toán:"));
+  let diemLy = parseFloat(prompt("Nhập điểm Lý:"));
+  let diemHoa = parseFloat(prompt("Nhập điểm Hóa:"));
+  let diemSinh = parseFloat(prompt("Nhập điểm Sinh:"));
+
+  // Kiểm tra điểm đầu vào
+  if (diemToan >= 0 && diemToan <= 10 &&
+      diemLy >= 0 && diemLy <= 10 &&
+      diemHoa >= 0 && diemHoa <= 10 &&
+      diemSinh >= 0 && diemSinh <= 10) {
+
+    // Tính điểm trung bình
+    let diemTrungBinh = (diemToan + diemLy + diemHoa + diemSinh) / 4;
+
+    // Xuất kết quả xếp loại
+    if (diemTrungBinh >= 9 && diemTrungBinh <= 10) {
+     console.log("Xếp loại: Giỏi");
+    } else if (diemTrungBinh >= 7 && diemTrungBinh < 9) {
+     console.log("Xếp loại: Khá");
+    } else if (diemTrungBinh >= 5 && diemTrungBinh < 7) {
+     console.log("Xếp loại: Trung Bình");
     } else {
-      console.log("Điểm nhập vào không hợp lệ!");
+     console.log("Xếp loại: Yếu");
     }
+
+  } else {
+    document.write("Điểm nhập vào không hợp lệ!");
   }
-  
-  // Hàm hiển thị menu
-  function hienThiMenu() {
-    console.log("MENU");
-    console.log("1. Tính điểm trung bình (switch case)");
-    console.log("2. Tính điểm trung bình (if else)");
-    console.log("0. Thoát");
-  }
-  
-  // Hàm chạy chương trình
-  function chayChuongTrinh() {
-    while (true) {
-      hienThiMenu();
-      let luaChon = prompt("Nhập lựa chọn của bạn 0,1,2:");
-  
-      switch (luaChon) {
-        case 1:
-          tinhDiemTrungBinh();
-          break;
-        case 2:
-          tinhDiemTrungBinh();
-          break;
-        case 0:
-          console.log("Chương trình kết thúc!");
-          return;
-        default:
-          console.log("Lựa chọn không hợp lệ!");
-      }
+}
+
+// Hàm hiển thị menu
+function hienThiMenu() {
+ console.log("MENU<br>");
+ console.log("1. Tính điểm trung bình (switch case)<br>");
+ console.log("2. Tính điểm trung bình (if else)<br>");
+ console.log("0. Thoát<br>");
+}
+
+// Hàm chạy chương trình
+function chayChuongTrinh() {
+  while (true) {
+    hienThiMenu();
+    let luaChon = parseInt(prompt("Nhập lựa chọn của bạn 0, 1 hoặc 2:"));
+
+    switch (luaChon) {
+      case 1:
+        tinhDiemTrungBinh();
+        break;
+      case 2:
+        tinhDiemTrungBinh();
+        break;
+      case 0:
+       console.log("Chương trình kết thúc!");
+        return;
+      default:
+       console.log("Lựa chọn không hợp lệ!");
     }
+
+    // Thêm dòng trắng
+   console.log("<br><br>");
   }
-  
-  // Chạy chương trình
-  chayChuongTrinh();
-  
+}
+
+// Chạy chương trình
+chayChuongTrinh();
